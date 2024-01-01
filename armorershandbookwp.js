3e5110e7c20a208fcaa3a30ec7189950c89eabda
Kaobara <dannymahmud1@gmail.com>
2024-01-02 00:47:49 +1100

    Removed unecessary tags. Only added additional new "weapon properties"

--- armorershandbookwp.js
+++ armorershandbookwp.js
@@ -1,17 +1,7 @@
 Hooks.on('ready', () => {
-	CONFIG.DND5E.weaponProperties['balanced'] = 'Balanced';
-  	CONFIG.DND5E.weaponProperties['critsharpened'] = 'Critical: Sharpened';
-	CONFIG.DND5E.weaponProperties['critsightpin'] = 'Critical: Sight Pin';
-	CONFIG.DND5E.weaponProperties['critspiked'] = 'Critical: Spiked';
-	CONFIG.DND5E.weaponProperties['runic'] = 'Runic';
-	CONFIG.DND5E.weaponProperties['woundkeen'] = 'Wounding: Keen';
-	CONFIG.DND5E.weaponProperties['woundoiledstring'] = 'Wounding: Oiled String';
-	CONFIG.DND5E.weaponProperties['brutal'] = 'Brutal';
-	CONFIG.DND5E.weaponProperties['enchanted'] = 'Enchanted ';
-	CONFIG.DND5E.weaponProperties['flanged'] = 'Flanged';
-	CONFIG.DND5E.weaponProperties['sawtoothed'] = 'Saw-Toothed';
-	CONFIG.DND5E.weaponProperties['superior'] = 'Superior';
-	CONFIG.DND5E.weaponProperties['arcane'] = 'Arcane';
-	CONFIG.DND5E.weaponProperties['masterwork'] = 'Masterwork';
+	CONFIG.DND5E.weaponProperties['bandolier'] = 'Bandolier';
+  	CONFIG.DND5E.weaponProperties['concealed'] = 'Concealed';
+	CONFIG.DND5E.weaponProperties['limited'] = 'Limited';
+	CONFIG.DND5E.weaponProperties['siege'] = 'Siege';
 	
 });
